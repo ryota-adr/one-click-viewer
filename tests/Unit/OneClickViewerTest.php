b1211f857904466051b3ddf27bf8ca7d25826a42
ryota_adr <pikyapikya12345@yahoo.co.jp>
2019-09-30 18:58:11 +0900

    add tests

--- tests/Unit/OneClickViewerTest.php
+++ tests/Unit/OneClickViewerTest.php
@@ -1,15 +1,100 @@
 <?php
-require_once('../../defines.php');
-require_once('../../src/OneClickViewer.php');
-require_once('../../tests/vendor/autoload.php');
 
 use PHPUnit\Framework\TestCase;
 
 class OneClickViewerTest extends TestCase
 {
-    public function testReplace()
+    protected $fullyQualifiedClassNames = [
+        'OneClickViewer\Tests\Int\InterfaceA',
+        'OneClickViewer\Tests\Int\InterfaceB',
+        'OneClickViewer\Tests\P\ParentA',
+        'OneClickViewer\Tests\Some\SomeClass',
+        'OneClickViewer\Tests\Ex\ExceptionA',
+    ];
+
+    protected $classArrs = [
+        [
+            'fullyQualifiedClassName' => 'OneClickViewer\Tests\Int\InterfaceA',
+            'end' => 'InterfaceA',
+            'alias' => ''
+        ], 
+        [
+            'fullyQualifiedClassName' => 'OneClickViewer\Tests\Int\InterfaceB',
+            'end' => 'InterfaceB',
+            'alias' => ''
+        ],
+        [
+            'fullyQualifiedClassName' => 'OneClickViewer\Tests\P\ParentA',
+            'end' => 'ParentA',
+            'alias' => ''
+        ],
+        [
+            'fullyQualifiedClassName' => 'OneClickViewer\Tests\Some\SomeClass',
+            'end' => 'SomeClass',
+            'alias' => ''
+        ],
+        [
+            'fullyQualifiedClassName' => 'OneClickViewer\Tests\Ex\ExceptionA',
+            'end' => 'ExceptionA',
+            'alias' => ''
+        ],
+        [
+            'fullyQualifiedClassName' => 'OneClickViewer\Tests\Some\Foo\SomeClass',
+            'end' => 'SomeClass',
+            'alias' => 'FooClass'
+        ],
+    ];
+
+    public static function setUpBeforeClass(): void
     {
-        $viewer = new OneClickViewer('../../.env');
+        define('ROOT', dirname(dirname(__DIR__)));
+
+        require(ROOT . '/defines.php');
+        require(ROOT . '/src/OneClickViewer.php');
+        require(ROOT . '/tests/vendor/autoload.php');
+    }
+
+    public function setUp(): void
+    {
+        $this->viewer = new OneClickViewer(ROOT . '/.env', 'OneClickViewer\Tests\TestClass');
+        $this->viewer->setHtml();
+        $this->code = $this->viewer->getHtml();
+    }
+
+    public function testReplaceFullyQualifiedClassName()
+    {
+        $classArrs = array_filter($this->classArrs, function ($classArr) {
+            return empty($classArr['alias']);
+        });
+        foreach ($classArrs as $classArr) {
+            preg_match_all('/' . preg_quote($classArr['fullyQualifiedClassName']) . '" role="link">' . preg_quote($classArr['fullyQualifiedClassName']) . '</', $this->code, $matches);
+            $this->assertTrue(!empty($matches[0]));
+        }
+    }
+
+    public function testReplaceAliasClasses()
+    {
+        $classArrs = array_filter($this->classArrs, function ($classArr) {
+            return !empty($classArr['alias']);
+        });
+
+        foreach ($classArrs as $classArr) {
+            preg_match_all(
+                '/' . preg_quote($classArr['fullyQualifiedClassName']) . '" role="link">' . preg_quote($classArr['fullyQualifiedClassName']) . ' as ' . $classArr['alias'] .'</',
+                $this->code,
+                $matchesAsAlias
+            );
+
+            $this->assertTrue(!empty($matchesAsAlias[0]));
+
+            preg_match_all(
+                '/' . preg_quote($classArr['fullyQualifiedClassName']) . '" role="link">' . $classArr['alias'] . '</',
+                $this->code,
+                $matchesAlias
+            );
+
+            $this->assertTrue(!empty($matchesAlias[0]));
+        }
     }
 }
 ?>

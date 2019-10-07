8a9612b2a7309b0af286d9e646d9a7487999aa5f
ryota_adr <pikyapikya12345@yahoo.co.jp>
2019-10-07 18:59:10 +0900

    add a pulgin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     entry: {
-        anyname: './src/scss/style.scss',
+        anyname: './src/scss/app.scss',
     },
     output: {
         path: path.resolve(__dirname, 'src/dist'),
@@ -13,6 +14,9 @@ module.exports = {
         rules: [
             {
                 test: /\.scss$/,
+                include: [
+                    path.resolve(__dirname, 'src/css/icomoon/fonts'),
+                ],
                 use: [
                     { loader: MiniCssExtractPlugin.loader },
                     { loader: 'css-loader' },
@@ -22,7 +26,25 @@ module.exports = {
         ]
     },
     plugins: [
-        new MiniCssExtractPlugin({filename: 'style.css'}),
+        new CopyPlugin([
+            {
+                from: 'src/css/icomoon/fonts/icomoon.eot',
+                to: 'fonts/icomoon.eot'
+            },
+            {
+                from: 'src/css/icomoon/fonts/icomoon.svg',
+                to: 'fonts/icomoon.svg'
+            },
+            {
+                from: 'src/css/icomoon/fonts/icomoon.ttf',
+                to: 'fonts/icomoon.ttf'
+            },
+            {
+                from: 'src/css/icomoon/fonts/icomoon.woff',
+                to: 'fonts/icomoon.woff'
+            }
+        ]),
+        new MiniCssExtractPlugin({filename: 'app.css'}),
     ],
     optimization: {
         minimizer: [new OptimizeCSSAssetsPlugin({})],
